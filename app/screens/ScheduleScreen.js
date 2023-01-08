0c73b7dcef62d937ab462bc17d8606513d0d1f6c
Brasen Xu <brasenxu@gmail.com>
2023-01-07 22:19:35 -0500

    Added colouring change for icons. Next step: add

--- app/screens/ScheduleScreen.js
+++ app/screens/ScheduleScreen.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import { Appbar, Avatar, Card, Paragraph, Text } from "react-native-paper";
@@ -7,6 +8,23 @@ import utils from "../config/utils";
 import colors from "../config/colors";
 
 function renderCard({ item }) {
+    const now = moment();
+    const times = item.time.split("-");
+    const startTime = moment(item.date + " " + times[0], "MMM Do h:mma");
+    const endTime = moment(item.date + " " + times[1], "MMM Do h:mma");
+
+    // console.log(startTime.format("dddd, MMMM Do YYYY, h:mma"));
+    // console.log(endTime.format("dddd, MMMM Do YYYY, h:mma"));
+
+    const getLabelStyle = () => {
+        if (now >= endTime) {
+            return styles.finishedAvatar;
+        } else if (now <= startTime) {
+            return styles.notFinishedAvatar;
+        }
+        return styles.inProgressAvatar;
+    };
+
     return (
         <Card style={styles.card} mode="outlined">
             <Card.Title
@@ -17,7 +35,7 @@ function renderCard({ item }) {
                 left={() => (
                     <Avatar.Text
                         size={40}
-                        style={styles.notFinishedAvatar}
+                        style={getLabelStyle()}
                         labelStyle={styles.weekdayAvatar}
                         label={item.weekday}
                     />
@@ -30,18 +48,8 @@ function renderCard({ item }) {
     );
 }
 
-function updateData(d) {
-    const currTime = utils.generateTime(d);
-    if (currTime === "12:00am") {
-        console.log("New Day schedule change");
-    }
-
-    console.log(currTime + " " + currTime.charAt(currTime.length - 3));
-}
-
 function ScheduleScreen(props) {
     const [data, setData] = useState([]);
-    const [isLoading, setLoading] = useState(false);
     const MINUTE_MS = 60000;
 
     const cards = [
@@ -49,100 +57,87 @@ function ScheduleScreen(props) {
             name: "MATH 135",
             date: "Jan 1st",
             weekday: "Mon",
-            time: "12:00pm - 12:50pm",
+            time: "12:00pm-12:50pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 136",
             date: "Jan 1st",
             weekday: "Mon",
-            time: "12:00pm - 12:50pm",
+            time: "12:00pm-12:50pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 137",
             date: "Jan 1st",
             weekday: "Mon",
-            time: "12:00pm - 12:50pm",
+            time: "12:00pm-12:50pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 138",
-            date: "Jan 1st",
+            date: "Jan 11th",
             weekday: "Mon",
-            time: "7:00pm - 8:50pm",
+            time: "7:00pm-8:50pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 139",
-            date: "Jan 1st",
+            date: "Jan 8th",
             weekday: "Mon",
-            time: "8:00pm - 9:00pm",
+            time: "8:00pm-9:00pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 140",
-            date: "Jan 1st",
+            date: "Jan 7th",
             weekday: "Mon",
-            time: "9:00pm - 10:50pm",
+            time: "9:00pm-10:50pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 141",
-            date: "Jan 1st",
+            date: "Jan 7th",
             weekday: "Mon",
-            time: "10:00pm - 11:00pm",
+            time: "10:00pm-11:00pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
         {
             name: "MATH 142",
-            date: "Jan 1st",
+            date: "Jan 8th",
             weekday: "Mon",
-            time: "11:00pm - 11:50pm",
+            time: "11:00pm-11:50pm",
             location: "MC 4021 (Mathematics and Computer)",
         },
     ];
 
-    const handleRefresh = () => {
-        // let API_URL = "https://jsonplaceholder.typicode.com/posts";
-        setLoading(true);
-        setData([
-            ...data,
-            ...cards,
-            // {
-            //     name: "MATH Brasen",
-            //     date: "Jan 1st",
-            //     weekday: "Mon",
-            //     time: "12:00pm - 12:50pm",
-            //     location: "MC 4021 (Mathematics and Computer)",
-            // },
-        ]);
-        setLoading(false);
-
-        // fetch(API_URL)
-        //   .then((res) => res.json())
-        //   .then((res) => {
-        //     console.log("refeshed");
-        //   })
-        //   .finally(() => setLoading(false));
-    };
-
     useEffect(() => {
-        const d = new Date();
+        const now = moment();
+
         let timeoutID, intervalID;
 
         timeoutID = setTimeout(() => {
-            updateData(new Date());
+            refreshCards(moment());
             intervalID = setInterval(() => {
-                updateData(new Date());
+                refreshCards(moment());
             }, MINUTE_MS);
-        }, (60 - d.getSeconds()) * 1000);
+        }, (60 - now.seconds()) * 1000);
 
         return () => {
             clearTimeout(timeoutID);
             clearInterval(intervalID);
         };
     }, []);
+
+    function refreshCards(now) {
+        const currTime = now.format("h:mma");
+        if (currTime === "12:00am") {
+            console.log("New Day schedule change");
+        }
+        setData([...data, ...cards]);
+        console.log("Data length: " + data.length);
+        console.log(currTime + " " + currTime.charAt(currTime.length - 3));
+    }
 
     return (
         <View style={sharedStyles.screen}>
@@ -157,12 +152,9 @@ function ScheduleScreen(props) {
                     onPress={() => console.log("add schedule")}
                 />
             </Appbar.Header>
-
             <FlatList
                 data={data}
                 renderItem={renderCard}
-                refreshing={isLoading}
-                onRefresh={handleRefresh}
                 showsVerticalScrollIndicator={false}
                 style={sharedStyles.mainContainer}
             />
